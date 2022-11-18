d874d1c74f7a1c0df43218196851a7cdace7ef3d
Vlad Ioffe <vlio20@gmail.com>
2022-11-18 08:21:55 +0200

    adding files

---
+++ applications/server/src/common/router/router.go
@@ -0,0 +1,57 @@
+package router
+
+import (
+	"lightup/src/common/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func handleReturn[T interface{}](c *gin.Context, dto *T, err error) {
+	var httpError *http.HttpError
+
+	if err != nil {
+		if _, ok := err.(*http.HttpError); ok {
+			httpError = err.(*http.HttpError)
+		} else {
+			httpError = http.GetHttpServerError(err)
+		}
+
+		c.JSON(httpError.StatusCode, httpError)
+		return
+	}
+
+	c.JSON(200, dto)
+}
+
+func HandleRequest[T interface{}](inv func(*gin.Context) (T, error)) func(*gin.Context) {
+	return func(c *gin.Context) {
+		dto, err := inv(c)
+		handleReturn(c, &dto, err)
+	}
+}
+
+func HandleBounding[T interface{}](inv func(*gin.Context, *T) (T, error)) func(*gin.Context) {
+	return func(c *gin.Context) {
+		var dto T
+
+		if err := c.ShouldBind(&dto); err != nil {
+			handleReturn[T](c, nil, &http.HttpError{
+				StatusCode:    400,
+				Message:       extractValidationError(err.Error()),
+				OriginalError: err,
+			})
+
+			return
+		}
+
+		resultDto, err := inv(c, &dto)
+		handleReturn(c, &resultDto, err)
+	}
+}
+
+func extractValidationError(msg string) string {
+	parts := strings.Split(msg, "Error:")
+
+	return parts[len(parts)-1]
+}
