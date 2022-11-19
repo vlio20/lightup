abebed4574aa9a8d52a23f08e15d85ff87c3b654
Vlad Ioffe <vlio20@gmail.com>
2022-11-19 23:18:32 +0200

    adding missing files

---
+++ applications/server/src/common/log/log.go
@@ -0,0 +1,54 @@
+package log
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Logger interface {
+	Info(args ...interface{})
+	Warn(args ...interface{})
+	Warning(args ...interface{})
+	Error(args ...interface{})
+	Panic(args ...interface{})
+	Fatal(args ...interface{})
+}
+
+type myFormatter struct {
+	module    string
+	formatter logrus.TextFormatter
+}
+
+func (f *myFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	var levelColor int
+	switch entry.Level {
+	case logrus.DebugLevel, logrus.TraceLevel:
+		levelColor = 31 // gray
+	case logrus.WarnLevel:
+		levelColor = 33 // yellow
+	case logrus.ErrorLevel, logrus.FatalLevel, logrus.PanicLevel:
+		levelColor = 31 // red
+	default:
+		levelColor = 36 // blue
+	}
+
+	return []byte(fmt.Sprintf("[%s] - [%s] - \x1b[%dm%s\x1b[0m - %s\n", entry.Time.Format(f.formatter.TimestampFormat), f.module, levelColor, strings.ToUpper(entry.Level.String()), entry.Message)), nil
+}
+
+func GetLogger(module string) Logger {
+	l := logrus.New()
+	l.SetFormatter(&myFormatter{
+		module: module,
+		formatter: logrus.TextFormatter{
+			FullTimestamp:          true,
+			TimestampFormat:        "2006-01-02 15:04:05",
+			ForceColors:            true,
+			DisableLevelTruncation: true,
+		},
+	})
+	l.SetReportCaller(false)
+
+	return l
+}
